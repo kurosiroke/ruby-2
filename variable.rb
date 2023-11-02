98d3b34bb2b38825e230654517b9cdeccc021c2d
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-11-02 13:14:41 +0000

    変数の変更

--- variable.rb
+++ variable.rb
@@ -1,2 +1,5 @@
 webcamp = "プログラミング学習" #(変数)webcamp＝(入れたいデータ)"プログラミング学習"　
 puts webcamp #(関数)puts webcamp (上記のコードで指定した変数を表示する引数)
+
+webcamp = "オンラインプログラミング学習" #変数に再度代入したことで中のデータが書き換えられる
+puts webcamp
