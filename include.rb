e32f2a41d105c60cae2ec4068fe136246e3b5fef
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-11-06 12:07:50 +0000

    含まれているか否か

---
+++ include.rb
@@ -0,0 +1,7 @@
+#引数に指定した文字列が含まれているかどうかを調べるinclude?メソッド
+
+puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+# .include?("含まれてほしいもの")
+
+#「WEBCAMPでプログラミング学習」の中には「WEBCAMP」が含まれているため、正しいことを表すtrueが出力
+#含まれていない場合はfalseが出力される。
